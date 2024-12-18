85bef1fced0ecacbcebc84d0f69b16747312b1c8
geo-tat <nosleep2night@yandex.ru>
2024-12-18 17:39:06 +0400

    add getSocksByRange function

--- src/main/java/com/geotat/socks/service/SocksService.java
+++ src/main/java/com/geotat/socks/service/SocksService.java
@@ -3,9 +3,12 @@ package com.geotat.socks.service;
 import com.geotat.socks.dto.SocksDtoIn;
 import com.geotat.socks.enums.Color;
 import com.geotat.socks.enums.ComparisonOperator;
+import com.geotat.socks.model.Socks;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.UUID;
 
 public interface SocksService {
@@ -18,4 +21,6 @@ public interface SocksService {
     void updateSocks(UUID id, @Valid SocksDtoIn dto);
 
     void uploadSocksBatch(MultipartFile file);
+
+    List<Socks> getSocksByPercentageRange(Integer minCottonPercentage, Integer maxCottonPercentage, String sortBy, boolean ascending);
 }
